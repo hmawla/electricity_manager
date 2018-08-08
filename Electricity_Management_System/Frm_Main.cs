c46e2c0fe28048d6acfc693bfa295552333da8d5
Pedro Marthon <pedro_marthon@live.com>
2018-08-08 15:05:47 +0300

    Customer datagrid view update

--- Electricity_Management_System/Frm_Main.cs
+++ Electricity_Management_System/Frm_Main.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialSkin;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Electricity_Management_System.OleDb_Tools;
 
 namespace Electricity_Management_System
 {
@@ -15,6 +17,35 @@ namespace Electricity_Management_System
         public Frm_Main()
         {
             InitializeComponent();
+
+            this.Load += new EventHandler(Main_OnLoad);
+            Tab_Navigator.SelectedIndexChanged += new EventHandler(Tab_Navigator_SelectedIndexChanged);
+            Btn_AddNewCustomer.Click += new EventHandler(Btn_AddNewCustomer_Click);
+
+            
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue800, Primary.Blue900, Primary.Blue500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        void Main_OnLoad(object sender, EventArgs e)
+        {
+
+        }
+
+        void Tab_Navigator_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Tab_Navigator.SelectedIndex == 1)
+            {
+                FillDGV(DGV_Customers, "SELECT customer_id AS [ID], customer_name AS [Name], customer_phone AS [Phone], counter_id AS [Counter], building_name & ', ' & street_name AS [Address] FROM customer, building, street WHERE customer.building_id = building.building_id AND building.street_id = street.street_id");
+            }
+        }
+
+        void Btn_AddNewCustomer_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
