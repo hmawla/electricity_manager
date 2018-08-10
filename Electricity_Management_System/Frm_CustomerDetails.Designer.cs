f6e9ebef2730a41b70b958e3206283f46f2eddfd
Pedro Marthon <pedro_marthon@live.com>
2018-08-10 11:06:47 +0300

    Quick details can now be shown upon row click

---
+++ Electricity_Management_System/Frm_CustomerDetails.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Electricity_Management_System
+{
+    partial class Frm_CustomerDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lbl_CustomerID = new System.Windows.Forms.Label();
+            this.Lbl_CustomerName = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Lbl_CustomerPhone = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Lbl_CounterID = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Lbl_Address = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Btn_Edit = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.Btn_Exit = new MaterialSkin.Controls.MaterialFlatButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Roboto", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer ID:";
+            // 
+            // Lbl_CustomerID
+            // 
+            this.Lbl_CustomerID.AutoSize = true;
+            this.Lbl_CustomerID.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_CustomerID.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_CustomerID.Location = new System.Drawing.Point(12, 95);
+            this.Lbl_CustomerID.Name = "Lbl_CustomerID";
+            this.Lbl_CustomerID.Size = new System.Drawing.Size(60, 29);
+            this.Lbl_CustomerID.TabIndex = 1;
+            this.Lbl_CustomerID.Text = "<ID>";
+            // 
+            // Lbl_CustomerName
+            // 
+            this.Lbl_CustomerName.AutoSize = true;
+            this.Lbl_CustomerName.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_CustomerName.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_CustomerName.Location = new System.Drawing.Point(13, 154);
+            this.Lbl_CustomerName.MaximumSize = new System.Drawing.Size(350, 29);
+            this.Lbl_CustomerName.Name = "Lbl_CustomerName";
+            this.Lbl_CustomerName.Size = new System.Drawing.Size(101, 29);
+            this.Lbl_CustomerName.TabIndex = 3;
+            this.Lbl_CustomerName.Text = "<Name>";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Roboto", 12F);
+            this.label3.Location = new System.Drawing.Point(13, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Customer Name:";
+            // 
+            // Lbl_CustomerPhone
+            // 
+            this.Lbl_CustomerPhone.AutoSize = true;
+            this.Lbl_CustomerPhone.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_CustomerPhone.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_CustomerPhone.Location = new System.Drawing.Point(14, 213);
+            this.Lbl_CustomerPhone.Name = "Lbl_CustomerPhone";
+            this.Lbl_CustomerPhone.Size = new System.Drawing.Size(105, 29);
+            this.Lbl_CustomerPhone.TabIndex = 5;
+            this.Lbl_CustomerPhone.Text = "<Phone>";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Roboto", 12F);
+            this.label4.Location = new System.Drawing.Point(14, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Customer Phone:";
+            // 
+            // Lbl_CounterID
+            // 
+            this.Lbl_CounterID.AutoSize = true;
+            this.Lbl_CounterID.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_CounterID.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_CounterID.Location = new System.Drawing.Point(223, 95);
+            this.Lbl_CounterID.Name = "Lbl_CounterID";
+            this.Lbl_CounterID.Size = new System.Drawing.Size(60, 29);
+            this.Lbl_CounterID.TabIndex = 7;
+            this.Lbl_CounterID.Text = "<ID>";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Roboto", 12F);
+            this.label6.Location = new System.Drawing.Point(223, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Counter ID:";
+            // 
+            // Lbl_Address
+            // 
+            this.Lbl_Address.AutoSize = true;
+            this.Lbl_Address.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_Address.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Address.Location = new System.Drawing.Point(15, 272);
+            this.Lbl_Address.MaximumSize = new System.Drawing.Size(350, 29);
+            this.Lbl_Address.Name = "Lbl_Address";
+            this.Lbl_Address.Size = new System.Drawing.Size(127, 29);
+            this.Lbl_Address.TabIndex = 9;
+            this.Lbl_Address.Text = "<Address>";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Roboto", 12F);
+            this.label5.Location = new System.Drawing.Point(15, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(142, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Customer Address:";
+            // 
+            // Btn_Edit
+            // 
+            this.Btn_Edit.AutoSize = true;
+            this.Btn_Edit.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Btn_Edit.Depth = 0;
+            this.Btn_Edit.Icon = null;
+            this.Btn_Edit.Location = new System.Drawing.Point(22, 335);
+            this.Btn_Edit.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Btn_Edit.Name = "Btn_Edit";
+            this.Btn_Edit.Primary = true;
+            this.Btn_Edit.Size = new System.Drawing.Size(50, 36);
+            this.Btn_Edit.TabIndex = 10;
+            this.Btn_Edit.Text = "Edit";
+            this.Btn_Edit.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Exit
+            // 
+            this.Btn_Exit.AutoSize = true;
+            this.Btn_Exit.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Btn_Exit.BackColor = System.Drawing.Color.White;
+            this.Btn_Exit.Depth = 0;
+            this.Btn_Exit.Icon = null;
+            this.Btn_Exit.Location = new System.Drawing.Point(79, 335);
+            this.Btn_Exit.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Btn_Exit.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Btn_Exit.Name = "Btn_Exit";
+            this.Btn_Exit.Primary = true;
+            this.Btn_Exit.Size = new System.Drawing.Size(63, 36);
+            this.Btn_Exit.TabIndex = 11;
+            this.Btn_Exit.Text = "Close";
+            this.Btn_Exit.UseVisualStyleBackColor = false;
+            // 
+            // Frm_CustomerDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(377, 383);
+            this.ControlBox = false;
+            this.Controls.Add(this.Btn_Exit);
+            this.Controls.Add(this.Btn_Edit);
+            this.Controls.Add(this.Lbl_Address);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Lbl_CounterID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Lbl_CustomerPhone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Lbl_CustomerName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Lbl_CustomerID);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(450, 383);
+            this.MinimumSize = new System.Drawing.Size(377, 383);
+            this.Name = "Frm_CustomerDetails";
+            this.Opacity = 0.9D;
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customer Card";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Lbl_CustomerID;
+        private System.Windows.Forms.Label Lbl_CustomerName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Lbl_CustomerPhone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Lbl_CounterID;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Lbl_Address;
+        private System.Windows.Forms.Label label5;
+        private MaterialSkin.Controls.MaterialRaisedButton Btn_Edit;
+        private MaterialSkin.Controls.MaterialFlatButton Btn_Exit;
+    }
+}
---
+++ Electricity_Management_System/Frm_CustomerDetails.cs
@@ -0,0 +1,58 @@
+﻿using MaterialSkin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Electricity_Management_System.OleDb_Tools;
+
+namespace Electricity_Management_System
+{
+    public partial class Frm_CustomerDetails : MaterialSkin.Controls.MaterialForm
+    {
+        private int CUSTOMER_ID;
+        public Frm_CustomerDetails(int CUSTOMER_ID)
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue800, Primary.Blue900, Primary.Blue500, Accent.LightBlue200, TextShade.WHITE);
+
+            this.CUSTOMER_ID = CUSTOMER_ID;
+            UpdateDetails();
+            
+            Btn_Exit.Click += new EventHandler(Btn_Exit_Click);
+            Btn_Edit.Click += new EventHandler(Btn_Edit_Click);
+
+        }
+
+        void UpdateDetails()
+        {
+            DataTable dt = new DataTable();
+            dt = ReadQueryOut("SELECT customer_name, customer_phone, floor, counter_id, building_name, street_name, region_name FROM customer c, building b, street s, region r WHERE c.building_id = b.building_id AND b.street_id = s.street_id AND s.region_id = r.region_id AND customer_id = " + CUSTOMER_ID);
+            Lbl_CustomerID.Text = CUSTOMER_ID.ToString();
+            Lbl_CustomerName.Text = dt.Rows[0].ItemArray[0].ToString();
+            Lbl_CustomerPhone.Text = dt.Rows[0].ItemArray[1].ToString();
+            Lbl_CounterID.Text = dt.Rows[0].ItemArray[3].ToString();
+            Lbl_Address.Text = "(" + dt.Rows[0].ItemArray[2].ToString() + ")" + " " + dt.Rows[0].ItemArray[4].ToString() + ", " + dt.Rows[0].ItemArray[5].ToString() + ", " + dt.Rows[0].ItemArray[6].ToString();
+        }
+
+        void Btn_Exit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        void Btn_Edit_Click(object sender, EventArgs e)
+        {
+            Frm_CustomerEdit frm = new Frm_CustomerEdit(CUSTOMER_ID);
+            frm.ShowDialog();
+            frm.Dispose();
+            UpdateDetails();
+        }
+    }
+}
